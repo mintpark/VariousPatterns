18df82fc0cd4ad7f8ade8b97f94273b98b632797
mint <hy.park@memebox.com>
2018-08-20 18:46:31 +0900

    feat: create first project

---
+++ VariousPatterns/VariousPatterns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VariousPatterns
+//
+//  Created by Hayoung Park on 2018. 8. 20..
+//  Copyright © 2018년 Hayoung Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

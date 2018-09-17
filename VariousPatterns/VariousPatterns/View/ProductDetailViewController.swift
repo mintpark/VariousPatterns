0d5ebcb76f0a9bcd79ece284b4c3c47deb001281
mint <hy.park@memebox.com>
2018-09-17 21:50:16 +0900

    feat: add command example
    어디서든지 command 날리면 뷰를 띄울수 있다!

--- VariousPatterns/VariousPatterns/View/ProductDetailViewController.swift
+++ VariousPatterns/VariousPatterns/View/ProductDetailViewController.swift
@@ -23,6 +23,17 @@ class ProductDetailViewController: UIViewController {
         presenter?.viewDidLoad()    // product didn't push yet
     }
     
+    @IBAction func showPrevDetailView(_ sender: Any) {
+        let item = Product(id: 111111, title: "prev", subTitle: "this is prev item via command", price: 111111)
+        self.post(notificationName: CommandProductDetail, product: item)
+        
+    }
+    
+    @IBAction func showNextDetailView(_ sender: Any) {
+        let item = Product(id: 222222, title: "next", subTitle: "this is next item via command", price: 222222)
+        self.post(notificationName: CommandProductDetail, product: item)
+    }
+    
     func showProductDetail(_ product: Product) {
         titleLabel.text = product.title
         subtitleLabel.text = product.subTitle
